c9196a4b8af80c308f988d34bbac96f37a448164
zhongxm <samingzhong@foxmail.com>
2022-05-20 11:02:47 +0800

    update version 1.0.1

--- LibA/Classes/CustomViewA.m
+++ LibA/Classes/CustomViewA.m
@@ -40,7 +40,7 @@
 
 
 - (void)initSubViews {
-    self.layer.cornerRadius = 20;
+    self.layer.cornerRadius = 20; // comment
     self.layer.masksToBounds = YES;
     UIView *topView = UIView.new;
     topView.backgroundColor = UIColor.redColor;
