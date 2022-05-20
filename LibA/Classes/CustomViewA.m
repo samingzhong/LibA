86d921d4d10045f851f7e562cb825a7dce8123f8
zhongxm <samingzhong@foxmail.com>
2022-05-20 10:12:22 +0800

    CustomViewA

--- LibA/Classes/CustomViewA.m
+++ LibA/Classes/CustomViewA.m
@@ -8,6 +8,16 @@
 #import "CustomViewA.h"
 #import "Masonry.h"
 
+@implementation CustomViewAModel
+@end
+
+
+@interface CustomViewA ()
+
+@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UIView *topView;
+@end
+
 @implementation CustomViewA
 
 /*
@@ -18,10 +28,9 @@
 }
 */
 
-
-- (instancetype)init
+- (instancetype)initWithFrame:(CGRect)frame
 {
-    self = [super init];
+    self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = UIColor.orangeColor;
         [self initSubViews];
@@ -37,8 +46,78 @@
     [topView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.right.top.mas_equalTo(0);
         make.height.mas_equalTo(30);
+        make.bottom.mas_equalTo(self.mas_bottom).offset(-100);
     }];
+    self.topView = topView;
+    UILabel *titleLabel = UILabel.new;
+    titleLabel.textColor = UIColor.whiteColor;
+    [topView addSubview:titleLabel];
+    titleLabel.backgroundColor = UIColor.purpleColor;
+    [titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.bottom.mas_equalTo(0);
+        make.centerX.mas_equalTo(0);
+    }];
+    self.titleLabel = titleLabel;
+    
+    #pragma mark test button
+    UIButton *btn = UIButton.new;
+    [self addSubview:btn];
+    [btn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(topView.mas_bottom);
+        make.centerX.mas_equalTo(0);
+    }];
+    [btn setTitle:@"click me" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+    
 }
+
+- (void)setTitle:(NSString *)title {
+    self.model.title = title;
+    self.titleLabel.text = self.model.title;
+    if (self.modelInfoChangeCallback) {
+        self.modelInfoChangeCallback(self.model);
+    }
+}
+
+- (void)btnClick {
+    [self setTitle:[NSString stringWithFormat:@"string:%@", @(arc4random()%100)]];
+    
+    [self changeSelfContentHeight];
+}
+
+- (void)setModel:(CustomViewAModel *)model {
+    _model = model;
+    self.titleLabel.text = self.model.title;
+}
+
+- (void)changeSelfContentHeight {
+    static BOOL expandBottom = NO;
+    if (!expandBottom) {
+        // 展开
+        [UIView animateWithDuration:0.25 animations:^{
+            [self.topView mas_updateConstraints:^(MASConstraintMaker *make) {
+                make.bottom.mas_equalTo(self.mas_bottom).offset(-120);
+            }];
+            self.layer.cornerRadius = 20;
+            [self.superview layoutIfNeeded];
+        }];
+    } else {
+        [UIView animateWithDuration:0.25 animations:^{
+            [self.topView mas_updateConstraints:^(MASConstraintMaker *make) {
+                make.bottom.mas_equalTo(self.mas_bottom).offset(-100);
+            }];
+            self.layer.cornerRadius = 0;
+            [self.superview layoutIfNeeded];
+        }];
+    }
+    expandBottom = !expandBottom;
+    self.model.showExpandBottom = expandBottom;
+    if (self.modelInfoChangeCallback) {
+        self.modelInfoChangeCallback(self.model);
+    }
+}
+
+
 
 
 @end
---
+++ README.md
@@ -0,0 +1,5 @@
+# 组建LibA说明
+
+CustomViewA是一个自定义的View组建，样式固定，支持设置model，且CustomViewA上的交互会影响model，并且model会回调给使用方。
+这就是一个简单的组建的设计和使用了。该组建依赖第三方库Masonry，在podSpec配置里设置依赖。
+
